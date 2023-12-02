b34d9b9e9154bd6b609f352d22a08e07c1cddd55
Saralyn <saralynl@bu.edu>
2023-12-02 17:36:21 -0500

    Added projects page and populated education data

---
+++ js/education.js
@@ -0,0 +1,28 @@
+const url = '/js/education.json'
+
+async function fetchEducation() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const educationOutput = document.getElementById('educationOutput');
+        educationOutput.innerHTML = buildEducationList(data);
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+fetchEducation();
+
+function buildEducationList(data) {
+    const educationList = data.map(item => {
+        const degree = item.degree;
+        return `<div id="educationOutput">
+        <ul id="education">    
+            <li><strong>${degree.type} in ${degree.program} | ${degree.school}</strong></li>
+            <li><i class="material-icons">calendar_month</i> ${degree.attended}</li>
+            <li><i class="material-icons">room</i>${degree.location}</li>
+        </ul>
+        </div>`
+    })
+    return `${educationList.join('')}`;
+}
