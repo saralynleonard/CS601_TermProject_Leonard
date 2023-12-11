fca64afb366222318b36126fc6b63c9f4ee4a8be
Saralyn <saralynl@bu.edu>
2023-12-11 18:47:38 -0500

    Added photos

--- js/gallery.js
+++ js/gallery.js
@@ -2,10 +2,12 @@ const app = Vue.createApp({
     data(){
         return {
             gallery: [
-                {image: './images/rainbow_falls.PNG', text: 'Rainbow falls in Hilo, HI - August 2018'}, 
+                {image: './images/rainbow_falls.PNG', text: 'Rainbow Falls in Hilo, HI - August 2018'}, 
                 {image: './images/hawaii_wall.png', text: 'Ancient wall in Southpoint, HI - August 2018'}, 
-                {image: './images/mauna_kea.png', text: 'Summit of Mauna Kea - August 2018'}, 
-                {image: './images/highland_cows.PNG', text: 'Highland cows in Nairn, Scotland - December 2017'}
+                {image: './images/mauna_kea.png', text: 'Sunset from the summit of Mauna Kea - August 2018'}, 
+                {image: './images/highland_cows.PNG', text: 'Highland cows in Nairn, Scotland - December 2017'},
+                {image: './images/kozushima.png', text: 'Ocean view from Kozushima Island, Japan - July 2019'},
+                {image: './images/sunset.png', text: 'Sunset at my childhood home'}
             ],
 
             galleryIndex: 0
